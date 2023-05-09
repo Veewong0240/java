a8e44b7d522eda050952707f234393d871b49b73
Veewong0240 <cwang18@uchicago.edu>
2023-05-09 11:14:02 -0500

    finish

--- SCRIPT/showimg.js
+++ SCRIPT/showimg.js
@@ -1,3 +1,16 @@
 function howdy(firstName){
     alert("Howdy " + firstName);
 }
+
+function displayMinute(){
+    var date = new Date();
+    var currentMinute = date.getMinutes();
+    
+    alert("Current minute:" + currentMinute)
+}
+
+function hideElement(){
+    var element = document.getElementById('body')
+
+    element.style.display = 'none';
+}
